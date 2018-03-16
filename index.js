4759d1614cfae8dcade5fe3d818a8e2027115921
Gonen Dukas <gonenduk@gmail.com>
2018-03-16 23:46:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const pid = parseInt(process.argv[2]);
+
+if (process.argv.length === 3 && pid > 0) {
+  try {
+    process._debugProcess(pid);
+    console.log('Debugger listener started successfully.');
+  } catch (err) {
+    console.log('Failed to send signal to process:', err.message);
+  }
+} else {
+  console.log('\nThis tool is used to start the debugger listener in a running Node.js process.');
+  console.log('\nUsage: latedebug [process id]');
+}
